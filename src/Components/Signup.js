b6309eeeb96cad6cd050f109f7a842167c1d0569
Viraj <60802568+vmp885@users.noreply.github.com>
2024-07-12 20:22:48 -0600

    Add files via upload
    Adding the initial file .js for signup page

---
+++ src/Components/Signup.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Signup.css"; // Ensure the path is correct
+
+function Signup() {
+  return (
+    <body class="signup-page">
+      <div className="signup">
+        <div className="card-signup">
+          <div className="card-body">
+            <h2 className="card-title text-center">Signup</h2> <br></br>
+            <form>
+            <div className="form-group">
+                
+              <label htmlFor="email">Username:</label>
+                <input
+                  type="text"
+                  className="form-control" 
+                  id="username"
+                  placeholder="Enter Username or Email"
+                />
+            </div>
+<br></br>
+            <div className="form-group">
+
+                <label htmlFor="password">Password:</label> 
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Enter password"
+                  />
+            </div>
+              <button type="submit" className="btn btn-success btn-block">
+                Signup
+              </button>
+              <p className="text-center mt-3">
+                <a href="#forgot-password" className="custom-link">Forgot Password?</a>
+                <br />
+                <a href="#forgot-username" className="custom-link">Forgot Username?</a>
+              </p>
+            </form>
+            <p className="text-center mt-3">
+              Don't have an account? {" "}
+              <a href="#login" className="custom-link">
+                Signup
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default Signup;
