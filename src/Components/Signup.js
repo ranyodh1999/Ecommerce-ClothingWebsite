f0be62f288bb527097de962d1c88f1c72b0c2329
WeiJia12689 <jiawei12689@gmail.com>
2024-07-16 23:15:09 -0600

    signup page code

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,55 +1,39 @@
-import React from "react";
-import "../CSS/App.css";
+import React from 'react';
+import './CSS/Signup.css'; // 确保路径正确
+
 function Signup() {
   return (
-    <body class="signup-page">
-      <div className="signup">
-        <div className="card-signup">
+    <div className="signup"> {/* 改为小写 */}
+      <div className="container">
+        <div className="card">
           <div className="card-body">
-            <h2 className="card-title text-center">Signup</h2> <br></br>
+            <h2 className="card-title text-center">Sign Up</h2>
             <form>
               <div className="form-group">
-                <label htmlFor="email">Username:</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="username"
-                  placeholder="Enter Username or Email"
-                />
+                <label htmlFor="email">Email:</label>
+                <input type="email" className="form-control" id="email" placeholder="Enter email" />
               </div>
-              <br></br>
+              <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input type="text" className="form-control" id="username" placeholder="Enter username" />
+              </div>
               <div className="form-group">
                 <label htmlFor="password">Password:</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  placeholder="Enter password"
-                />
+                <input type="password" className="form-control" id="password" placeholder="Enter password" />
               </div>
-              <button type="submit" className="btn btn-success btn-block">
-                Signup
-              </button>
-              <p className="text-center mt-3">
-                <a href="#forgot-password" className="custom-link">
-                  Forgot Password?
-                </a>
-                <br />
-                <a href="#forgot-username" className="custom-link">
-                  Forgot Username?
-                </a>
-              </p>
+              <div className="form-group">
+                <label htmlFor="confirmPassword">Confirm Password:</label>
+                <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm password" />
+              </div>
+              <button type="submit" className="btn btn-success btn-block">Sign Up</button>
             </form>
             <p className="text-center mt-3">
-              Don't have an account?{" "}
-              <a href="#login" className="custom-link">
-                Signup
-              </a>
+              Already have an account? <a href="#login">Login</a>
             </p>
           </div>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
 
