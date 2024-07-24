9ee21602bd72f5968ea67e64c033ec60c296ea0a
Ranyodh Singh <133093093+ranyodh1999@users.noreply.github.com>
2024-07-23 22:45:09 -0700

    Add files via upload

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,59 +1,8 @@
 import React from "react";
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBIcon,
-} from "mdb-react-ui-kit";
+import "../CSS/Footer.css";
 
-export default function Footer() {
-  return (
-    <MDBFooter bgColor="light" className="text-center text-lg-start text-muted">
-      <section className="">
-        <MDBContainer className="text-center text-md-start mt-5">
-          <MDBRow className="mt-3">
-            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">
-                <MDBIcon icon="gem" className="me-3" />
-                T-SHIRT CLOTHING WEBSITE
-              </h6>
-              <p>footer content.</p>
-            </MDBCol>
-
-            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
-              <p>
-                <a href="Signup" className="text-reset">
-                  Instagram
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-              <p>
-                <MDBIcon icon="home" className="me-2" />
-                Regina, SK
-              </p>
-              <p>
-                <MDBIcon icon="envelope" className="me-3" />
-                projectcs476@gmail.com
-              </p>
-              <p>
-                <MDBIcon icon="phone" className="me-3" /> + 1 999 999 999
-              </p>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </section>
-
-      <div
-        className="text-center p-4"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-      >
-        © 2024 Copyright: UofR Project Team
-      </div>
-    </MDBFooter>
-  );
+function Footer() {
+  return <section className="footer">@ Copyright 2024</section>;
 }
+
+export default Footer;
