7086ef47e2f88036903faf5fcfa486757b465b99
Ranyodh Singh <133093093+ranyodh1999@users.noreply.github.com>
2024-07-25 21:11:33 -0700

    server.js (updated)

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const UserModel = require("./models/User");
+
 require("dotenv").config();
 
 const app = express();
@@ -21,11 +23,17 @@ mongoose
     process.exit(1);
   });
 
-app.use("/api/users", require("./routes/users"));
 app.use("/api/checkout", require("./routes/orders"));
 app.use("/api/custom", require("./routes/customs"));
+app.use("/api/users", require("./routes/users"));
+
+app.get("api/users", (res) => {
+  UserModel.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Server is running on port ${PORT}");
+  console.log(`Server is running on port ${PORT}`);
 });
