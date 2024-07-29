bee06446643498a6f35a4b1b0daa262ab45ef885
singhmukul411 <102141014+singhmukul411@users.noreply.github.com>
2024-07-28 21:20:26 -0600

    Add files via upload

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const UserModel = require("./models/User");
-
 require("dotenv").config();
 
 const app = express();
@@ -23,17 +21,11 @@ mongoose
     process.exit(1);
   });
 
+app.use("/api/users", require("./routes/users"));
 app.use("/api/checkout", require("./routes/orders"));
 app.use("/api/custom", require("./routes/customs"));
-app.use("/api/users", require("./routes/users"));
-
-app.get("api/users", (res) => {
-  UserModel.find()
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("Server is running on port ${PORT}");
 });
