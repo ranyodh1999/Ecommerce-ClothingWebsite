5c521cb82cc6aae6a313fcabd79b05e9de775773
Ranyodh Singh <133093093+ranyodh1999@users.noreply.github.com>
2024-08-02 14:39:39 -0700

    Adding search bar in the home page for filter

--- src/Shopping/Home.js
+++ src/Shopping/Home.js
@@ -1,22 +1,41 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Container, Row, Col, Image, Card } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 import { ShopContext } from "../Context/shopContext";
 import { Products } from "./Products";
 import "../CSS/App.css";
 
-import headerImage from "../Asset/Header.jpg";
+function SearchBar({ filterText, onFilterTextChange }) {
+  return (
+    <form>
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)}
+        className="form-control mb-3"
+      />
+    </form>
+  );
+}
 
 const Home = () => {
   const { addToCart, cartItems } = useContext(ShopContext);
+  const [filterText, setFilterText] = useState("");
+
+  const filteredProducts = Products.filter((product) =>
+    product.productName.toLowerCase().includes(filterText.toLowerCase())
+  );
 
   return (
     <Container>
-      <Card>
-        <Image src={headerImage} width={1300} height={175} />
-      </Card>
+      <div className="search">
+        {" "}
+        <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
+      </div>
+
       <Row>
-        {Products.map((product) => (
+        {filteredProducts.map((product) => (
           <Col xs={6} md={3} key={product.id}>
             <Link to={product.detailPage} className="image-link">
               <div className="image-container">
